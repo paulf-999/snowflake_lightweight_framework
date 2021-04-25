4c661c1be037f9a62e5f32feb618f6d97e047320
Paul Fry <paul.fry@versent.com.au>
2021-04-26 07:13:14 +1000

    revisions to baseline role permissions

--- account_objects/role/v1_roles.sql
+++ account_objects/role/v1_roles.sql
@@ -14,7 +14,7 @@ DROP ROLE IF EXISTS &{PROGRAM}_&{ENV}_SF_STAGE_ADMIN;
 --create the user roles
 USE ROLE SECURITYADMIN;
 CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_DBA COMMENT = 'DBA for the &{PROGRAM} DBs. This role owns all of the &{PROGRAM} databases and their constraints.';
-CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_DATA_LOADER COMMENT = 'Used for loading data into &{PROGRAM} tables from Snowflake stages/ext tables/MVs.';
+CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_DATA_LOADER COMMENT = 'Used for loading data into &{PROGRAM} tables from Snowflake stages/ext tables/tables.';
 CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_DEVELOPER COMMENT = 'Used to load data into the &{PROGRAM} tables. Also operates on the data for updates, inserts, truncates etc.';
 CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_VISUALISER COMMENT = 'Used by reporting tool service account roles.';
 CREATE ROLE IF NOT EXISTS &{PROGRAM}_&{ENV}_ANALYST COMMENT = 'Used for QA and analysis. Main purpose is querying and interacting with the data. Mostly read-only operations.';
